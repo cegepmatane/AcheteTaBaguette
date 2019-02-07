0a93e51d3dcb9a31195ce9cb56b0704ca5e23923
Yonnet Maxime <Yonnet.maxime@laposte.net>
2019-02-07 14:55:13 -0500

    #15 Ajout de la date et de la classe personne

--- source/achetetabaguette_fr/inscription.php
+++ source/achetetabaguette_fr/inscription.php
@@ -16,36 +16,36 @@ include("header.html");
                 <legend>Inscription</legend>
                 <div class="form-group">
                     <label class="col-form-label" for="nom">Nom</label>
-                    <input type="text" class="form-control" id="nom">
+                    <input type="text" class="form-control" id="nom" required>
                 </div>
                 <div class="form-group">
                   <label class="col-form-label" for="prenom">Prénom  </label>
-                    <input type="text" class="form-control" id="Prenom">
+                    <input type="text" class="form-control" id="Prenom" required>
                 </div>
                 <div class="form-group">
                     <label class="col-form-label" for="date">Date de Naissance </label>
-                      <input type='text' class="form-control" placeholder="DD/MM/YYY" />
+                      <input type='text' class="form-control" placeholder="DD/MM/YYY" required/>
                 </div>
                 <div class="form-group">
                     <label for="mail">Mail</label>
-                    <input type="email" class="form-control" id="mail" aria-describedby="emailHelp">
+                    <input type="email" class="form-control" id="mail" aria-describedby="emailHelp" required>
                 </div>
                 <div class="form-group">
                     <label for="motDePasse1">Mot de passe</label>
-                    <input type="password" class="form-control" id="motDePasse1">
+                    <input type="password" class="form-control" id="motDePasse1" required>
                 </div>
                 <div class="form-group">
                     <label for="motDePasse2">Confirmer mot de passe</label>
-                    <input type="password" class="form-control" id="motDePasse2">
+                    <input type="password" class="form-control" id="motDePasse2" required>
                 </div>
                 <div class="row justify-content-md-center">
                   <label class="form-check-label" for="defaultCheck1">
                     J'ai lu, compris et accepté les termes d'utilisation
                   </label>
-                    <input type="checkbox" name="condition" value="agreed">
+                    <input type="checkbox" name="condition" value="agreed" required>
                 </div>
                     <div class="row justify-content-md-center">
-                      <button type="submit" class="btn btn-primary">Envoyer</button>
+                      <button type="submit" class="btn btn-primary">Envoyer </button>
                     </div>
                 </fieldset>
             </form>
