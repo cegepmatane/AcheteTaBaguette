7a42748dee03c2e03300dc348c1927a6edfc5211
Téo BRYER <teocalme@gmail.com>
2019-02-13 19:13:30 -0500

    Ajout php admin #27

---
+++ source/achete_ta_baguette_fr/administration/vue/index.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once("entete-fragment-admin.php");
+require_once("page-liste-produit.php");
+require_once("pied-de-page-fragment.php");
+
+/*
+Un tableau associatif en clé valeur permet de définir les éléments dynamiques
+de la page qui ne sont pas en lien avec les données du modèle. En le changeant
+de type en (object), il est plus facilement utilisable par l'IDE et permet les
+suggestions automatiques des attributs de l'objet.
+*/
+
+$page = (object)
+    [
+    "titre" => "Page index",
+    "titrePrincipal" => "Le titre principal H1",
+    "itemMenuActif" => "accueil",
+    "isConnected" => true,
+
+    "nom" => "Affin",
+    "prenom" => "Jean-Yves",
+    "courriel" => "jean-yves@affin.net",
+    "rue" => "616 Av. St-Redempteur",
+    "ville" => "Matane",
+    "province" => "Québec",
+    "codePostal" => "G4W 0H2",
+    "pays" => "Canada",
+    "id_personne" => 1
+    ];
+
+/*
+La fonction afficherPage produit le code HTML de la page d'accueil.
+Le paramètre $page permet aux fragments de page inclus d'utiliser les
+éléments dynamiques propres à la page d'accueil.
+*/
+
+function afficherPage($page = null){
+
+    // En cas d'erreur avec le paramètre $page, un objet $page vide est créé.
+    if(!is_object($page)) $page = (object)[];
+
+        afficherEntete($page); ?>
+
+       
+                <?php afficherContenu($page); ?>
+           
+
+          
+            
+      
+
+        <?php afficherPiedDePage($page); ?>
+
+<?php
+}
+
+afficherPage($page);
+
+// EOF
---
+++ source/achete_ta_baguette_fr/administration/vue/pied-de-page-fragment.php
@@ -0,0 +1,61 @@
+<?php
+
+function afficherPiedDePage($page = null) {
+    if(!is_object($page)) return;
+?>
+
+        <!-- Pied de page -->
+        <div class="row mt-4">
+            <div class="col-md-12 bg-primary">
+            
+                <footer class="footer">
+
+                    <!-- contenu footer -->
+                    <div class="row pt-4 justify-content-around align-items-center"> 
+
+                        <!-- Adresse -->
+                        <div class="col-md-2">
+                            <a  alt="Adresse" href="https://www.google.ca/maps/place/Rue+Saint+Antoine,+Saint-Jean-sur-Richelieu,+QC,+Canada/@45.276341,-73.2546747,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc9a281ec4537ab:0xa60b61f4cc5beeb4!8m2!3d45.2763372!4d-73.252486">
+                                <img 
+                                alt="Achete ta baguette" 
+                                src="../../commun/illustration/adresse.png"
+                                class="img-fluid"
+                                href="https://www.w3schools.com/html/"
+                                />
+                            </a>
+                        </div>
+
+                        <!-- Nom site -->
+                        <div class="col-md-3">
+                            <img 
+                            alt="Achete ta baguette" 
+                            src="../..//commun/illustration/achete.png"
+                            class="img-fluid"
+                            href="https://www.w3schools.com/html/"
+                            />
+                        </div>
+
+                        <!-- GoogleMaps -->
+                        <div class="col-md-2">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d969375.0014612963!2d-73.48320803120802!3d45.04924948487961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9a281ec4537ab%3A0xa60b61f4cc5beeb4!2sRue+Saint+Antoine%2C+Saint-Jean-sur-Richelieu%2C+QC!5e0!3m2!1sfr!2sca!4v1549935431409" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        </div>
+
+                    </div>
+                        
+                    <!-- Credits -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p class="text-center text-light">Site réalisé dans le cadre d'un projet Web au Cégep de Matane 2019 &copy AcheteTaBaguette </p>
+                        </div>
+                    </div>
+                        
+                </footer>
+            </div>
+        </div><!-- Fin pied de page -->
+
+    </div><!-- Fin container -->
+  </body>
+</html>
+
+<?php
+}
