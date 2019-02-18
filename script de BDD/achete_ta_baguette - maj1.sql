51cf99a3d5d7ad460adfc1e20aa530a075aec271
Téo BRYER <teocalme@gmail.com>
2019-02-18 16:52:22 -0500

    Mise à jour BDD #30

--- script de BDD/achete_ta_baguette - maj1.sql
+++ script de BDD/achete_ta_baguette - maj1.sql
@@ -1,3 +1,3 @@
 INSERT INTO `CATEGORIE`(`idCategorie`, `label`, `description`) VALUES (3,'Autre',null);
 
-INSERT INTO `PRODUIT`(`idProduit`, `nom`, `description`, `prix`, `stock`, `idCategorie`, `srcImage`) VALUES (5,'Sachet de bonbons','Ne vous inquiétez pas pour les carries',2,3,30,'bonbon.png');
+INSERT INTO `PRODUIT`(`idProduit`, `nom`, `description`, `prix`, `stock`, `idCategorie`, `srcImage`) VALUES (5,'Sachet de bonbons','Ne vous inquiétez pas pour les carries',2,30,3,'bonbon.png');
