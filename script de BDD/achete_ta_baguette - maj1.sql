dcbf06411d45454f9ff321148fd99b1983510e88
AlexandreCM <alexandre.mezeray@gmail.com>
2019-02-20 18:38:12 -0500

    renommage script sql + correction

--- script de BDD/achete_ta_baguette - maj1.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `CATEGORIE`(`idCategorie`, `label`, `description`) VALUES (3,'Autre',null);
-
-INSERT INTO `PRODUIT`(`idProduit`, `nom`, `description`, `prix`, `stock`, `idCategorie`, `srcImage`) VALUES (5,'Sachet de bonbons','Ne vous inquiétez pas pour les carries',2,30,3,'bonbon.png');
