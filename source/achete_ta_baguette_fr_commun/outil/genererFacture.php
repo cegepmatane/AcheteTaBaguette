5d0303bec0c60078cda56d6c8aefd5308ad476f7
Arthur Cheramy <arthur.cheramy@gmail.com>
2019-03-11 16:15:36 -0400

    Ajout coloration des lignes tu tableau #57

--- source/achete_ta_baguette_fr_commun/outil/genererFacture.php
+++ source/achete_ta_baguette_fr_commun/outil/genererFacture.php
@@ -12,7 +12,7 @@ class PDF extends FPDF
         $this->Ln(20);
     }
 
-    public function LoadData($file)
+    public function chargerData($file)
     {
         // Lecture des lignes du fichier
         $lines = file($file);
@@ -24,8 +24,17 @@ class PDF extends FPDF
         return $data;
     }
 
+    public function isColored($i)
+    {
+        if ($i % 2 == 0) {
+            $this->SetFillColor(224, 226, 229);
+        } else {
+            $this->SetFillColor(255, 255, 255);
+        }
+    }
+
 // Tableau amélioré
-    public function ImprovedTable($header, $data)
+    public function genererTableau($header, $data)
     {
         $prixTotal = 0;
 
@@ -38,36 +47,47 @@ class PDF extends FPDF
 
         $this->Ln();
         // Données
+        $i = 0;
         foreach ($data as $row) {
-            $this->Cell($w[0], 6, utf8_decode($row[0]), 'LR', 0, 'R');
-            $this->Cell($w[1], 6, $row[1] . " " . chr(128), 'LR', 0, 'R');
-            $this->Cell($w[2], 6, number_format($row[2], 0, ',', ' '), 'LR', 0, 'R');
-            $this->Cell($w[3], 6, $row[1] * $row[2] . " " . chr(128), 'LR', 0, 'R');
+            $this->isColored($i);
+            $i++;
+
+            $this->Cell($w[0], 6, utf8_decode($row[0]), 'LR', 0, 'R', true);
+            $this->Cell($w[1], 6, $row[1] . " " . chr(128), 'LR', 0, 'R', true);
+            $this->Cell($w[2], 6, number_format($row[2], 0, ',', ' '), 'LR', 0, 'R', true);
+            $this->Cell($w[3], 6, $row[1] * $row[2] . " " . chr(128), 'LR', 0, 'R', true);
             $prixTotal += $row[1] * $row[2];
             $this->Ln();
         }
+
         $this->Cell($w[0], 6, "", "LRB", 0, 'R');
         $this->Cell($w[1], 6, "", "LRB", 'R');
         $this->Cell($w[2], 6, "", 'LR', 0, 'R');
         $this->Cell($w[3], 6, "", 'LR', 0, 'R');
         $this->Ln();
 
+        $this->isColored($i);
+        $i++;
         $this->Cell($w[0], 6, "", '', 0, 'R');
         $this->Cell($w[1], 6, "", '', 0, 'R');
-        $this->Cell($w[2], 6, "Prix total HT", 1, 0, 'R');
-        $this->Cell($w[3], 6, $prixTotal . " " . chr(128), 1, 0, 'R');
+        $this->Cell($w[2], 6, "Prix total HT", 1, 0, 'R', true);
+        $this->Cell($w[3], 6, $prixTotal . " " . chr(128), 1, 0, 'R', true);
         $this->Ln();
 
+        $this->isColored($i);
+        $i++;
         $this->Cell($w[0], 6, "", '', 0, 'R');
         $this->Cell($w[1], 6, "", '', 0, 'R');
-        $this->Cell($w[2], 6, "Taxes (20%)", 1, 0, 'R');
-        $this->Cell($w[3], 6, $prixTotal * 0.2 . " " . chr(128), 1, 0, 'R');
+        $this->Cell($w[2], 6, "Taxes (20%)", 1, 0, 'R', true);
+        $this->Cell($w[3], 6, $prixTotal * 0.2 . " " . chr(128), 1, 0, 'R', true);
         $this->Ln();
 
+        $this->isColored($i);
+        $i++;
         $this->Cell($w[0], 6, "", '', 0, 'R');
         $this->Cell($w[1], 6, "", '', 0, 'R');
-        $this->Cell($w[2], 6, "Prix total", 1, 0, 'R');
-        $this->Cell($w[3], 6, $prixTotal * 1.2 . " " . chr(128), 1, 0, 'R');
+        $this->Cell($w[2], 6, "Prix total", 1, 0, 'R', true);
+        $this->Cell($w[3], 6, $prixTotal * 1.2 . " " . chr(128), 1, 0, 'R', true);
         $this->Ln();
 
         // Trait de terminaison
@@ -80,8 +100,8 @@ $pdf = new PDF();
 // Titres des colonnes
 $header = array('Item', 'Prix Unitaire', utf8_decode(Quantité), 'Prix Total');
 // Chargement des données
-$data = $pdf->LoadData('data.txt');
+$data = $pdf->chargerData('data.txt');
 $pdf->SetFont('Arial', '', 14);
 $pdf->AddPage();
-$pdf->ImprovedTable($header, $data);
+$pdf->genererTableau($header, $data);
 $pdf->Output('I', "Facture", true);
