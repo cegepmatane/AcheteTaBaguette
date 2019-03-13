878349ecde60abd1d96bc24fed2c731b6502a435
AlexandreCM <alexandre.mezeray@gmail.com>
2019-03-13 00:25:45 -0400

    #77 affichage des data de la base sur la page

--- source/achete_ta_baguette_fr/utilisateur/vue/panier-fragment.php
+++ source/achete_ta_baguette_fr/utilisateur/vue/panier-fragment.php
@@ -5,11 +5,20 @@
  * Date: 2019-03-06
  * Time: 16:44
  */
+require_once(CHEMIN_RACINE_COMMUN . "/modele/Produit.php");
+require_once(CHEMIN_RACINE_COMMUN . "/modele/Panier.php");
+require_once(CHEMIN_RACINE_COMMUN . "/accesseur/AccesseurProduit.php");
+require_once(CHEMIN_RACINE_COMMUN . "/accesseur/AccesseurPanier.php");
 
 function afficherPanier($page = null){
     $page->titre = "Panier";
-?>
 
+    $accesseurPanier = new AccesseurPanier();
+    $listePanier = $accesseurPanier->reupererPanier($_SESSION[Client::EMAIL]);
+    $accesseurProduit = new AccesseurProduit();
+    $totalPrix = null;
+
+?>
     <div class="row mb-3">
         <div class="col-md-12">
             <h1><?= $page->titre ?></h1>
@@ -26,18 +35,26 @@ function afficherPanier($page = null){
                     <th>Prix total</th>
                 </tr>
 
-                <?php for ($i = 0; $i < 2; $i++) { ?>
+                <?php foreach($listePanier as $panier) {
+                    $produit = $accesseurProduit->recupererProduitParId($panier->getIdProduit());
+                    $totalPrix += $produit->prix*$panier->getNbProduit(); ?>
                 <tr>
                     <td></td>
-                    <td><p><span class="font-weight-bold"><?= $nb=2 ?></span> x unProduit</p></td>
-                    <td><p><?= $prix=2 ?> CND$</p></td>
-                    <td><p><?= $prix*$nb ?> CND$</p></td>
+                    <td><p><span class="font-weight-bold"><?= $panier->getNbProduit() ?></span> x <?= $produit->nom ?></p></td>
+                    <td><p><?= $produit->prix ?> CND$</p></td>
+                    <td><p><?= $produit->prix*$panier->getNbProduit() ?> CND$</p></td>
                 </tr>
                 <?php } ?>
 
             </table>
         </div>
         <div class="col-md-3 text-center">
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    <p class="font-weight-bold">Prix Total HT : <?= $totalPrix ?></p>
+                    <p class="font-weight-bold">Prix Total TTC : <?= $ttc = round($totalPrix + $totalPrix*(0.05+0.09975),2) ?></p>
+                </div>
+            </div>
             <div class="row mb-3">
                 <div class="col-md-12">
                     <a href="#" class="btn btn-danger">Commander</a>
--- source/achete_ta_baguette_fr_commun/accesseur/AccesseurPanier.php
+++ source/achete_ta_baguette_fr_commun/accesseur/AccesseurPanier.php
@@ -16,6 +16,7 @@ class AccesseurPanier
     private const SUBTITUT_NB_PRODUIT = ":" .Panier::NB_PRODUIT;
 
     private static $AJOUTER_PANIER = "INSERT INTO PANIER(".Panier::EMAIL_CLIENT.", ".Panier::ID_PRODUIT.", ".Panier::NB_PRODUIT.") VALUES (".self::SUBTITUT_EMAIL_CLIENT.", ".self::SUBTITUT_ID_PRODUIT.", ".self::SUBTITUT_NB_PRODUIT.");";
+    private static $RECUPERER_PANIER_CLIENT_PAR_EMAIL = "SELECT ".Panier::ID_PRODUIT.", ".Panier::NB_PRODUIT." FROM PANIER WHERE emailClient LIKE ".self::SUBTITUT_EMAIL_CLIENT.";";
 
     private static $connexion = null;
     public function __construct()
@@ -39,6 +40,25 @@ class AccesseurPanier
 
             return false;
 
+        }
+    }
+
+    public function reupererPanier($emailClient)
+    {
+        try {
+            $panier = [];
+
+            $requete = self::$connexion->prepare(self::$RECUPERER_PANIER_CLIENT_PAR_EMAIL);
+            $requete->bindValue(self::SUBTITUT_EMAIL_CLIENT, $emailClient, PDO::PARAM_STR);
+            $requete->execute();
+            $listeEnregistrement = $requete->fetchAll(PDO::FETCH_OBJ);
+            foreach ($listeEnregistrement as $enregistrement) {
+                $panier[] = new Panier($enregistrement);
+            }
+            return $panier;
+
+        } catch (PDOException $e) {
+            return false;
         }
     }
 }
