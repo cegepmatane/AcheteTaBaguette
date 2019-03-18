3df52599cf962a9d1c40d255148078e03595a932
Téo BRYER <teocalme@gmail.com>
2019-03-18 03:03:13 -0400

    Commentaires, nettoyage, ré indentation #27

--- source/achete_ta_baguette_fr/administration/vue/index.php
+++ source/achete_ta_baguette_fr/administration/vue/index.php
@@ -1,119 +1,113 @@
-<?php
+  <?php
 
-require_once("../../commun/vue/entete-fragment.php");
-require_once("../../commun/vue/pied-de-page-fragment.php");
-require_once("page-liste-produit.php");
-require_once(CHEMIN_RACINE_ADMINISTRATION . "/action/action-page-liste-produit.php");
+  require_once("../../commun/vue/entete-fragment.php");
+  require_once("../../commun/vue/pied-de-page-fragment.php");
+  require_once("page-liste-produit.php");
+  require_once(CHEMIN_RACINE_ADMINISTRATION . "/action/action-page-liste-produit.php");
 
-/*
-Un tableau associatif en clé valeur permet de définir les éléments dynamiques
-de la page qui ne sont pas en lien avec les données du modèle. En le changeant
-de type en (object), il est plus facilement utilisable par l'IDE et permet les
-suggestions automatiques des attributs de l'objet.
- */
 
-/*
-La fonction afficherPage produit le code HTML de la page d'accueil.
-Le paramètre $page permet aux fragments de page inclus d'utiliser les
-éléments dynamiques propres à la page d'accueil.
- */
-afficherEntete($page);
+  afficherEntete($page);
 
-?>
-<!-- Centre de page -->
+  ?>
+  <!-- Centre de page -->
 
-<div class="row mb-3 text-center">
+  <div class="row mb-3 text-center">
     <!-- Contenu -->
     <div class="col">
 
-      <table class="table ">
+      <table class="table "><!-- Table produits -->
 
-        <thead>
-            <tr>
-              <th  scope="col">Image</th>
-              <th  scope="col">Catégorie</th>
-              <th  scope="col">Produit</th>
-              <th  scope="col">Description</th>
-              <th  scope="col">Prix</th>
-              <th  scope="col">Stock</th>
-              <th  scope="col">Modifier</th>
-              <th  scope="col">Supprimer</th>
+        <thead> <!--  Intitulés des colonnes -->
+          <tr>
+            <th  scope="col">Image</th>
+            <th  scope="col">Catégorie</th>
+            <th  scope="col">Produit</th>
+            <th  scope="col">Description</th>
+            <th  scope="col">Prix</th>
+            <th  scope="col">Stock</th>
+            <th  scope="col">Modifier</th>
+            <th  scope="col">Supprimer</th>
           </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($page->listeProduits as $produit) {?>
-            <tr>
-              <td><input type="image" src="illustration/add-image.png" /></td>
-              <td> <?=  recupererLabelCategorieParProduit($page, $produit); ?> </td>
-              <td> <?= $produit->getNom(); ?></td>
-              <td> <?=  $produit->getDescription(); ?></td>
-              <td> <?=  $produit->getPrix(); ?></td>
-              <td> <?=  "" .$produit->getStock() . ""; ?></td>
-              <td> <a href="modifier/<?= $produit->getIdproduit(); ?>">Modifier</a></td>
-              <td> <form method="post" action="index.php">
-                <input type="submit" src="../illustration/trash.png"  name="action-supprimer-produit" id="button" value="bar"></input>
-                <input type="hidden" name="idProduit" value="<?= $produit->getIdProduit(); ?>"></input>
+        </thead><!-- Fin Intitulés des colonnes -->
+
+        <tbody> <!--  Affichage liste de produits existants ( lignes du tableau ) -->
+         <?php foreach ($page->listeProduits as $produit) {?> 
+
+
+          <tr>
+            <td><input type="image" src="illustration/add-image.png" /></td>
+            <td> <?=  recupererLabelCategorieParProduit($page, $produit); ?> </td>
+            <td> <?= $produit->getNom(); ?></td>
+            <td> <?=  $produit->getDescription(); ?></td>
+            <td> <?=  $produit->getPrix(); ?></td>
+            <td> <?=  "" .$produit->getStock() . ""; ?></td>
+            <td> <a href="modifier/<?= $produit->getIdproduit(); ?>">Modifier</a></td>
+            <td> <form method="post" action="index.php">
+              <input type="submit" src="../illustration/trash.png"  name="action-supprimer-produit" id="button" value="bar"></input>
+              <input type="hidden" name="idProduit" value="<?= $produit->getIdProduit(); ?>"></input>
             </form ></td>
-        </tr>
-    <?php }?>
-</tbody>
-</table>
+          </tr>
 
-            <form method="post" enctype="multipart/form-data">
-                <table class="table ">
-                    <thead>
-                    <tr>
-                        <th scope="col">Image</th>
-                        <th scope="col">Cat&eacute;gorie</th>
-                        <th scope="col">Produit</th>
-                        <th scope="col">Description</th>
-                        <th scope="col">Prix</th>
-                        <th scope="col">Stock</th>
-                        <th scope="col"></th>
+        <?php }?>
+      </tbody> <!--  Fin Affichage liste de produits existants ( lignes du tableau ) -->
+    </table> <!-- Fin Table produits -->
 
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>
-                            <div class="container">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                                <input type="file" name="image"/>
-                                        </div>
-                                        <img id='img-upload'/>
-                                    </div>
-                                </div>
-                            </div>
+    <form method="post" enctype="multipart/form-data">  <!-- Formulaire ajout produit -->
+      <table class="table ">  <!-- Tableau ajout produit -->
+        <thead><!--  Intitulés des colonnes -->
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Cat&eacute;gorie</th>
+            <th scope="col">Produit</th>
+            <th scope="col">Description</th>
+            <th scope="col">Prix</th>
+            <th scope="col">Stock</th>
+            <th scope="col"></th>
+          </tr>
+        </thead><!-- Fin  Intitulés des colonnes -->
+
+        <tbody><!-- Affichage des chammps pour l'ajout -->
+          <tr>
+            <td>
+              <div class="container">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <div class="input-group">
+                      <input type="file" name="image"/>
+                    </div>
+                    <img id='img-upload'/>
+                  </div>
+                </div>
+              </div>
             </form>
+          </div>
+          <img id='img-upload'/>
         </div>
-        <img id='img-upload'/>
+      </div>
     </div>
-</div>
-</div>
-</div>
+  </div>
 
-           <!--  <input type="image" src="illustration/add-image.png" name="image" /></td> -->
-          <!--  <td ><input type="text" class="form-control"  name="categorie"></td> -->
-          <td>  <select class="custom-select" name="<?= PRODUIT::ID_CATEGORIE ?>"  >
-              <option value="" selected disabled hidden>Choisir catégorie</option>
-              <?php foreach ($page->listeCategorie as $categorie) {?>
-                 <option value= "<?= $categorie->getIdCategorie() ?>"> <?= $categorie->getLabel() ?></option>
-             <?php }?>
 
-         </select></td>
-         <td ><input type="text" class="form-control"  name=" <?= PRODUIT::NOM ?>"></td>
-         <td><input type="text" class="form-control"  name=" <?= PRODUIT::DESCRIPTION ?>"></td>
-         <td><input type="text" class="form-control"  name=" <?= PRODUIT::PRIX ?>"></td>
-         <td><input type="text" class="form-control" name="  <?=PRODUIT::STOCK ?>"></td>
-         <td ><input  class="add" type="submit" src="../illustration/add.png" name="action-ajouter-produit"/></td>
-     </tr>
+  <td>  
+    <select class="custom-select" name="<?= PRODUIT::ID_CATEGORIE ?>"  >
+      <option value="" selected disabled hidden>Choisir catégorie</option>
+      <?php foreach ($page->listeCategorie as $categorie) {?>
+       <option value= "<?= $categorie->getIdCategorie() ?>"> <?= $categorie->getLabel() ?></option>
+     <?php }?>
+    </select>
+ </td>
 
- </tbody>
+ <td><input type="text" class="form-control"  name=" <?= PRODUIT::NOM ?>"></td>
+ <td><input type="text" class="form-control"  name=" <?= PRODUIT::DESCRIPTION ?>"></td>
+ <td><input type="text" class="form-control"  name=" <?= PRODUIT::PRIX ?>"></td>
+ <td><input type="text" class="form-control" name="  <?=PRODUIT::STOCK ?>"></td>
+ <td><input  class="add" type="submit" src="../illustration/add.png" name="action-ajouter-produit"/></td>
+</tr>
+
+</tbody> <!-- FIN Affichage des chammps pour l'ajout -->
 </table>
 </form>
 </div><!-- Fin du contenu -->
 </div><!-- Fin centre de page -->
 <?php afficherPiedDePage($page);
-// EOF
+  // EOF
