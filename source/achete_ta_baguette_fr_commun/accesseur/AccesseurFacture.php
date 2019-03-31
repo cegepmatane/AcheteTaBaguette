bbeba86a80767c75ad66927dc039b661e699d6e9
AlexandreCM <alexandre.mezeray@gmail.com>
2019-03-30 21:57:58 -0400

    #84 requete facture et facturer ok dans methode ajouterFacture()

--- source/achete_ta_baguette_fr_commun/accesseur/AccesseurFacture.php
+++ source/achete_ta_baguette_fr_commun/accesseur/AccesseurFacture.php
@@ -12,10 +12,16 @@ require_once(CHEMIN_RACINE_COMMUN . "/modele/Facture.php");
 class AccesseurFacture
 {
     private const SUBTITUT_EMAIL_CLIENT = ":" . Facture::EMAIL_CLIENT;
-    private const SUBTITUT_ID_PRODUIT = ":" . Facture::ID_PRODUIT;
-    private const SUBTITUT_NB_PRODUIT = ":" . Facture::NB_PRODUIT;
+    private const SUBTITUT_ID_FACTURE = ":" . Facture::ID_FACTURE;
+    private const SUBTITUT_DATE_ACHAT = ":" . Facture::DATE_ACHAT;
+    private const SUBTITUT_PRIX_HT = ":" . Facture::PRIX_HT;
+    private const SUBTITUT_PRIX_TTC = ":" . Facture::PRIX_TTC;
 
-    private static $AJOUTER_FACTURE = "INSERT INTO FACTURER(".Facture::EMAIL_CLIENT.", ".Facture::ID_PRODUIT.", ".Facture::NB_PRODUIT.") VALUES (".self::SUBTITUT_EMAIL_CLIENT.", ".self::SUBTITUT_ID_PRODUIT.", ".self::SUBTITUT_NB_PRODUIT.");";
+    private static $AJOUTER_FACTURE = "INSERT INTO FACTURE(".Facture::DATE_ACHAT.", ".Facture::PRIX_HT.", ".Facture::PRIX_TTC.") VALUES (".self::SUBTITUT_DATE_ACHAT.", ".self::SUBTITUT_PRIX_HT.", ".self::SUBTITUT_PRIX_TTC.");";
+    private static $AJOUTER_FACTURER = "INSERT INTO FACTURER(".Facture::EMAIL_CLIENT.", ".Facture::FACTURER.") VALUES (".self::SUBTITUT_ID_FACTURE.", ".self::SUBTITUT_ID_FACTURE.");";
+    private static $AJOUTER_LISTE_PRODUIT = "INSERT INTO LISTE_PRODUIT_FACTURE (produit, facture, quantite) VALUES ();";
+
+    private static $RECUPERER_FACTURE_PAR_ID = "INSERT INTO Facturer(".Facture::ID_FACTURE.", ".Facture::EMAIL_CLIENT.", ".Facture::ID_PRODUIT.", ".Facture::QUANTITE.") VALUES (".self::SUBTITUT_ID_FACTURE.", ".self::SUBTITUT_EMAIL_CLIENT.", ".self::SUBTITUT_ID_PRODUIT.", ".self::SUBTITUT_NB_PRODUIT.");";
 
     private static $connexion = null;
     public function __construct()
@@ -23,20 +29,26 @@ class AccesseurFacture
         if (!self::$connexion) self::$connexion = BaseDeDonnee::getConnexion();
     }
 
-    public function ajouterFacture(Panier $panier)
+    public function ajouterFacture(Facture $facture)
     {
         try {
 
             $requete = self::$connexion->prepare(self::$AJOUTER_FACTURE);
-            $requete->bindValue(self::SUBTITUT_EMAIL_CLIENT, $panier->getEmailClient(), PDO::PARAM_STR);
-            $requete->bindValue(self::SUBTITUT_ID_PRODUIT, $panier->getIdProduit(), PDO::PARAM_STR);
-            $requete->bindValue(self::SUBTITUT_NB_PRODUIT, $panier->getNbProduit(), PDO::PARAM_STR);
-
+            $requete->bindValue(self::SUBTITUT_DATE_ACHAT, $facture->getDateAchat(), PDO::PARAM_STR);
+            $requete->bindValue(self::SUBTITUT_PRIX_HT, $facture->getPrixHT(), PDO::PARAM_STR);
+            $requete->bindValue(self::SUBTITUT_PRIX_TTC, $facture->getPrixTTC(), PDO::PARAM_STR);
             $requete->execute();
-            return self::$AJOUTER_FACTURE;
+            $facture->setIdFacture(self::$connexion->lastInsertId());
+
+            $requete = self::$connexion->prepare(self::$AJOUTER_FACTURER);
+            $requete->bindValue(self::SUBTITUT_EMAIL_CLIENT, $facture->getEmailClient(), PDO::PARAM_STR);
+            $requete->bindValue(self::SUBTITUT_ID_FACTURE, $facture->getIdFacture(), PDO::PARAM_STR);
+            $requete->execute();
+
+            return $facture;
 
         } catch (PDOException $e) {
-            return self::$AJOUTER_FACTURE;
+            return false;
         }
 
     }
--- source/achete_ta_baguette_fr_commun/modele/Facture.php
+++ source/achete_ta_baguette_fr_commun/modele/Facture.php
@@ -3,11 +3,14 @@
 class Facture
 {
 
+    public const FACTURER = "facture";
+    public const EMAIL_CLIENT = "emailClient";
     public const ID_FACTURE = "idFacture";
     public const DATE_ACHAT = "dateAchat";
     public const PRIX_HT = "prixHT";
     public const PRIX_TTC = "prixTTC";
 
+    private $emailClient;
     private $idFacture;
     private $dateAchat;
     private $prixHT;
@@ -18,10 +21,22 @@ class Facture
         if (!is_object($attribut)) {
             $attribut = (object) [];
         }
-        $this->setDateAchat($attribut->dateAchat ?? "");
+        $this->setEmailClient($attribut->emailClient ?? "");
+        $this->setDateAchat($attribut->dateAchat ?? "NOW()");
         $this->setPrixHT($attribut->prixHT ?? 0);
         $this->setPrixTTC($attribut->prixTTC ?? 0);
         $this->setIdFacture($attribut->idFacture ?? null);
+    }
+
+    public function getEmailClient()
+    {
+        return $this->emailClient;
+    }
+
+    public function setEmailClient($emailClient)
+    {
+        $this->emailClient = filter_var($emailClient, FILTER_SANITIZE_STRING);
+        return true;
     }
 
     public function getIdFacture()
