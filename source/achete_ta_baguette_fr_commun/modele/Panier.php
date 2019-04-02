014cfb18f5710a4243826bd7a9b4f6dd5522649c
AlexandreCM <alexandre.mezeray@gmail.com>
2019-04-01 23:45:53 -0400

    #83 model panier ok

--- source/achete_ta_baguette_fr_commun/modele/Panier.php
+++ source/achete_ta_baguette_fr_commun/modele/Panier.php
@@ -10,15 +10,16 @@ class Panier
 {
     public const EMAIL_CLIENT = "emailClient";
     public const ID_PRODUIT = "idProduit";
-    public const NB_PRODUIT = "nbProduit";
+    public const QUANTITE = "quantite";
+    public const PRIX_HT = "prixHT";
+    public const PRIX_TTC = "prixTTC";
 
     private $emailClient;
     private $idProduit;
-    private $nbProduit;
-
-    private static $LISTE_MESSAGE_ERREUR = [];
-
-    private $listeMessageErreurActif = [];
+    private $quantite;
+    private $listeProduit;
+    private $prixHT;
+    private $prixTTC;
 
     public function __construct(object $attribut)
     {
@@ -28,50 +29,7 @@ class Panier
 
         $this->setEmailClient($attribut->emailClient ?? null);
         $this->setIdProduit($attribut->idProduit ?? null);
-        $this->setNbProduit($attribut->nbProduit ?? null);
-    }
-
-    public function isValide($champ = null)
-    {
-
-        if (null == $champ) {
-
-            $this->setEmailClient($this->emailClient);
-            $this->setIdProduit($this->idProduit);
-            $this->setNbProduit($this->nbProduit);
-
-            return $this->listeMessageErreurActif;
-
-        }
-
-        $nomClasse = get_class();
-        $constante = "$nomClasse::" . strtoupper($champ);
-        if (!defined($constante)) {
-            return false;
-        }
-
-        return !isset($this->listeMessageErreurActif[$champ]);
-
-    }
-
-    public static function getListeMessageErreur()
-    {
-        if (empty(self::$LISTE_MESSAGE_ERREUR))
-        {
-            self::$LISTE_MESSAGE_ERREUR =
-                [
-                    "emailClient-vide" => "Le emailClient ne doit pas être vide",
-                    "idProduit-vide" => "Le idProduit ne doit pas être vide",
-                    "nbProduit-vide" => "Le nbProduit ne doit pas être vide",
-                ];
-        }
-
-        return self::$LISTE_MESSAGE_ERREUR;
-    }
-
-    public function getListeMessageErreurActif($champ)
-    {
-        return $this->listeMessageErreurActif[$champ] ?? [];
+        $this->setQuantite($attribut->quantite ?? null);
     }
 
     public function getEmailClient()
@@ -81,14 +39,6 @@ class Panier
 
     public function setEmailClient($emailClient)
     {
-        // Validation en premier
-        if (empty($emailClient))
-        {
-            $this->listeMessageErreurActif[self::EMAIL_CLIENT][] = self::getListeMessageErreur()['emailClient-vide'];
-            return false;
-        }
-
-        // Nettoyage en second
         $this->emailClient = filter_var($emailClient, FILTER_SANITIZE_STRING);
         return true;
     }
@@ -100,34 +50,57 @@ class Panier
 
     public function setIdProduit($idProduit)
     {
-        // Validation en premier
-        if (empty($idProduit))
-        {
-            $this->listeMessageErreurActif[self::ID_PRODUIT][] = self::getListeMessageErreur()['idProduit-vide'];
-            return false;
-        }
-
-        // Nettoyage en second
         $this->idProduit = filter_var($idProduit, FILTER_SANITIZE_STRING);
         return true;
     }
 
-    public function getNbProduit()
+    public function getQuantite()
     {
-        return $this->nbProduit;
+        return $this->quantite;
     }
 
-    public function setNbProduit($nbProduit)
+    public function setQuantite($quantite)
     {
-        // Validation en premier
-        if (empty($nbProduit))
-        {
-            $this->listeMessageErreurActif[self::NB_PRODUIT][] = self::getListeMessageErreur()['nbProduit-vide'];
-            return false;
-        }
-
-        // Nettoyage en second
-        $this->nbProduit = filter_var($nbProduit, FILTER_SANITIZE_STRING);
+        $this->quantite = filter_var($quantite, FILTER_SANITIZE_STRING);
         return true;
     }
+
+    public function getListeProduit()
+    {
+        return $this->listeProduit;
+    }
+
+    public function setListeProduit($listeProduit)
+    {
+        $this->listeProduit = $listeProduit;
+    }
+
+    public function addProduit($produit)
+    {
+        $this->listeProduit += $produit;
+//        $this->setPrixHT($produit->getPrix(), $this->getQuantite());
+//        $this->setPrixTTC(0.2);
+    }
+
+    public function getPrixHT()
+    {
+        return $this->prixHT;
+    }
+
+    public function setPrixHT($prix, $quantite)
+    {
+        $this->prixHT = $this->prixHT + $prix * $quantite;
+    }
+
+    public function getPrixTTC()
+    {
+        return $this->prixTTC;
+    }
+
+    public function setPrixTTC($taxe)
+    {
+        $this->prixTTC = $this->getPrixHT() * $taxe;
+    }
+
+
 }
