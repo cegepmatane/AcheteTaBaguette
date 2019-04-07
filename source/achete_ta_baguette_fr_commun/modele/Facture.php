81cc0dd7b26eebc4c9436812ba9090e1d1c37dca
AlexandreCM <alexandre.mezeray@gmail.com>
2019-04-07 13:05:18 -0400

    #84 maj du model facture

--- source/achete_ta_baguette_fr_commun/modele/Facture.php
+++ source/achete_ta_baguette_fr_commun/modele/Facture.php
@@ -3,14 +3,14 @@
 class Facture
 {
 
-    public const EMAIL_CLIENT = "emailClient";
     public const ID_FACTURE = "idFacture";
+    public const EMAIL_CLIENT = "emailClient";
     public const DATE_ACHAT = "dateAchat";
     public const PRIX_HT = "prixHT";
     public const PRIX_TTC = "prixTTC";
 
-    private $emailClient;
     private $idFacture;
+    private $emailClient;
     private $dateAchat;
     private $prixHT;
     private $prixTTC;
@@ -18,26 +18,14 @@ class Facture
 
     public function __construct(object $attribut)
     {
-        if (!is_object($attribut)) {
-            $attribut = (object) [];
-        }
+        if (!is_object($attribut)) $attribut = (object) [];
+
         $this->setIdFacture($attribut->idFacture ?? null);
         $this->setEmailClient($attribut->emailClient ?? "");
         $this->setDateAchat($attribut->dateAchat ?? "NOW()");
-        $this->setPrixHT($attribut->prixHT ?? 0);
-        $this->setPrixTTC($attribut->prixTTC ?? 0);
-        $this->setListeProduit($attribut->listeProduit ?? null);
-    }
-
-    public function getEmailClient()
-    {
-        return $this->emailClient;
-    }
-
-    public function setEmailClient($emailClient)
-    {
-        $this->emailClient = filter_var($emailClient, FILTER_SANITIZE_STRING);
-        return true;
+        $this->setPrixHT($attribut->prixHT ?? "");
+        $this->setPrixTTC($attribut->prixTTC ?? "");
+        $this->setListeProduit($attribut ?? null);
     }
 
     public function getIdFacture()
@@ -48,6 +36,17 @@ class Facture
     public function setIdFacture($idFacture)
     {
         $this->idFacture = filter_var($idFacture, FILTER_SANITIZE_STRING);
+        return true;
+    }
+
+    public function getEmailClient()
+    {
+        return $this->emailClient;
+    }
+
+    public function setEmailClient($emailClient)
+    {
+        $this->emailClient = filter_var($emailClient, FILTER_SANITIZE_STRING);
         return true;
     }
 
@@ -64,7 +63,7 @@ class Facture
 
     public function getPrixHT()
     {
-        return $this->prixHT;
+        return round($this->prixHT, 2);
     }
 
     public function setPrixHT($prixHT)
@@ -75,7 +74,7 @@ class Facture
 
     public function getPrixTTC()
     {
-        return $this->prixTTC;
+        return round($this->prixTTC, 2);
     }
 
     public function setPrixTTC($prixTTC)
@@ -89,10 +88,10 @@ class Facture
         return $this->listeProduit;
     }
 
-    public function setListeProduit(ListeProduitFacture $listeProduit)
+    public function setListeProduit($article)
     {
-        $this->listeProduit = filter_var($listeProduit, FILTER_SANITIZE_STRING);
-        return true;
+        $article = new Article($article);
+        $this->listeProduit[] = $article;
     }
 
 }
